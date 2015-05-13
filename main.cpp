96877e09c292ed60d444d8e0d1d1fcb3d680f5fc
AzHaR <azharakbar@gmail.com>
2015-05-13 19:51:00 +0530

    Added Space Removal In The Expression For Uniformity

--- main.cpp
+++ main.cpp
@@ -4,12 +4,24 @@
 using namespace std ;
 
 extern int para_match_test ( char expr[] ) ;
+extern void space_remove ( char expr [] ) ;
 
 void main ()
 {
 	char expr[100] = "" ;
 
+	cout << " >> Enter The Mathematical Expression To Be Evaluated : ";
 	cin.getline ( expr , 100 ) ;
+
+	//Enclosing The Entire Expression In ( )
+	char temp[100] = "(";
+	strcat ( temp , expr ) ;
+	strcat ( temp , ")" ) ;
+	strcpy ( expr , temp ) ;
+
+	space_remove ( expr ) ;
+
+	cout << endl << " >> The Updated Expression Is : " << expr ;
 
 	if ( para_match_test ( expr ) )
 		cout<<endl<<"MATCHING CORRECT";
---
+++ space_remover.cpp
@@ -0,0 +1,17 @@
+extern void space_remove ( char expr [] )
+{
+	int i = 0 , j = 0 ;
+
+	for ( i = 0 ; expr[i] != '\0' ; ++i )
+	{
+		if ( expr[i] == ' ' )
+		{
+			j = i ;
+			while ( expr[j] != '\0' )
+			{
+				expr[j] = expr[j+1] ;
+				++j ;
+			}
+		}
+	}
+}
