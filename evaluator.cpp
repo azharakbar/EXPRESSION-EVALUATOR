049d45afa8469cf0a188c0b6c7173eebe7cd2077
AzHaR <azharakbar@gmail.com>
2015-05-18 12:11:57 +0530

    Changed subtraction syntax while popping :)

--- evaluator.cpp
+++ evaluator.cpp
@@ -7,8 +7,8 @@ using namespace std ;
 
 struct stack 
 {
-	double operand ;
-	int opcode ;
+    double operand ;
+    int opcode ;
 };
 
 stack A[100] ;
@@ -16,88 +16,88 @@ int top = -1 ;
 
 extern double evaluator ( char expr[] )
 {
-	double result = 0 ,  temp = 0 ;
-	int i = 0 , opcode = 0 , dec = -1 , j = 0 , ten = 1 ;
+    double result = 0 ,  temp = 0 ;
+    int i = 0 , opcode = 0 , dec = -1 , j = 0 , ten = 1 ;
 
-	while ( expr[i] != '\0' )
-	{
-		if ( (expr[i] >= 48 && expr[i] <= 57) )
-		{
-			if ( dec != -1 ) {  ++dec ; }
-			temp = ( temp *10 ) + ( (int)(expr[i]) - 48 ) ;
-		}
-		
-		else if ( expr[i] == '.' )
-		{
-			dec = 0 ;
-		}
+    while ( expr[i] != '\0' )
+    {
+        if ( (expr[i] >= 48 && expr[i] <= 57) )
+        {
+            if ( dec != -1 ) {  ++dec ; }
+            temp = ( temp *10 ) + ( (int)(expr[i]) - 48 ) ;
+        }
+        
+        else if ( expr[i] == '.' )
+        {
+            dec = 0 ;
+        }
 
-		else if ( expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/' )
-		{
-			if ( dec != -1 )
-			{
-				ten = 1 ;
-				for ( j = 1 ; j <= dec ; ++j )
-					ten = ten*10 ;
-				temp = temp / ten ;
-				dec = -1 ; 
-			}
-			
-			if ( !result )
-				result = temp ;
-			else
-			{
-				if ( opcode == 1 ) result += temp ;
-				else if ( opcode == 2 ) result -= temp ;
-				else if ( opcode == 3 ) result *= temp ;
-				else if ( opcode == 4 ) result /= temp ;
-			}
+        else if ( expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/' )
+        {
+            if ( dec != -1 )
+            {
+                ten = 1 ;
+                for ( j = 1 ; j <= dec ; ++j )
+                    ten = ten*10 ;
+                temp = temp / ten ;
+                dec = -1 ; 
+            }
+            
+            if ( !result )
+                result = temp ;
+            else
+            {
+                if ( opcode == 1 ) result += temp ;
+                else if ( opcode == 2 ) result -= temp ;
+                else if ( opcode == 3 ) result *= temp ;
+                else if ( opcode == 4 ) result /= temp ;
+            }
 
-			temp = 0 ;
-			if ( expr [i] == '+' ) opcode = 1 ;
-			else if ( expr [i] == '-' ) opcode = 2 ;
-			else if ( expr [i] == '*' ) opcode = 3 ;
-			else if ( expr [i] == '/' ) opcode = 4 ;
-		}
+            temp = 0 ;
+            if ( expr [i] == '+' ) opcode = 1 ;
+            else if ( expr [i] == '-' ) opcode = 2 ;
+            else if ( expr [i] == '*' ) opcode = 3 ;
+            else if ( expr [i] == '/' ) opcode = 4 ;
+        }
 
-		else if ( expr[i] == '(' )
-		{
-			if ( i && result )
-			{
-				A[++top].operand = result ;
-				A[top].opcode = opcode ;
-				result = temp = opcode = 0 ;
-			}
-		}
+        else if ( expr[i] == '(' )
+        {
+            if ( i && result )
+            {
+                A[++top].operand = result ;
+                A[top].opcode = opcode ;
+                result = temp = opcode = 0 ;
+            }
+        }
 
-		else if ( expr[i] == ')' )
-		{
-				if ( dec != -1 )
-				{
-					ten = 1 ;
-					for ( j = 1 ; j <= dec ; ++j )
-						ten = ten*10 ;
-					temp = temp / ten ;
-					dec = 0 ; 
-				}			
-				if ( opcode == 1 ) result += temp ;
-				else if ( opcode == 2 ) result -= temp ;
-				else if ( opcode == 3 ) result = result * temp ;
-				else if ( opcode == 4 ) result /= temp ;
-				opcode = 0 ;
-			if ( top != -1 )
-			{
-				if ( A[top].opcode == 1 ) result += A[top--].operand ;
-				else if ( A[top].opcode == 2 ) result -= A[top--].operand ;
-				else if ( A[top].opcode == 3 ) result *= A[top--].operand ;
-				else if ( A[top].opcode == 4 ) result /= A[top--].operand ;
-			}
-			if ( !result ) result = temp ;
-			temp = 0 ;
-		}
-		++i ;
-	}
+        else if ( expr[i] == ')' )
+        {
+                if ( dec != -1 )
+                {
+                    ten = 1 ;
+                    for ( j = 1 ; j <= dec ; ++j )
+                        ten = ten*10 ;
+                    temp = temp / ten ;
+                    dec = 0 ; 
+                }           
+                if ( opcode == 1 ) result += temp ;
+                else if ( opcode == 2 ) result -= temp ;
+                else if ( opcode == 3 ) result = result * temp ;
+                else if ( opcode == 4 ) result /= temp ;
+                opcode = 0 ;
+            if ( top != -1 )
+            {
+                if ( A[top].opcode == 1 ) result += A[top--].operand ;
+				else if ( A[top].opcode == 2 ) result = A[top--].operand - result ;
+                else if ( A[top].opcode == 3 ) result *= A[top--].operand ;
+                else if ( A[top].opcode == 4 ) result /= A[top--].operand ;
+            }
+            if ( !result ) result = temp ;
+            temp = 0 ;
+        }
+        ++i ;
+    }
 
-	
-	return result ;
+    
+    return result ;
 }
